e00fb7140f8ab0209c9b0bd2b277e2499883ffc9
Vovavolik <florinapool@gmail.com>
2026-01-27 13:35:29 +0200

    Initial clean setup

--- api/login.js
+++ api/login.js
@@ -1,17 +1,3 @@
 export default function handler(req, res) {
-  const clientId = process.env.DISCORD_CLIENT_ID;
-  const redirectUri = process.env.DISCORD_REDIRECT_URI;
-
-  if (!clientId || !redirectUri) {
-    return res.status(500).send("Missing env variables");
-  }
-
-  const discordAuthUrl =
-    "https://discord.com/api/oauth2/authorize" +
-    `?client_id=${clientId}` +
-    "&response_type=code" +
-    "&scope=identify" +
-    `&redirect_uri=${encodeURIComponent(redirectUri)}`;
-
-  res.redirect(discordAuthUrl);
+  res.send("API LOGIN OK");
 }

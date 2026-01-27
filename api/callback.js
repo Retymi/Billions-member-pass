7e0b952bd971fbc514ca101cc3f2a449e5890bb6
Vovavolik <florinapool@gmail.com>
2026-01-27 13:55:29 +0200

    Add Discord OAuth

--- api/callback.js
+++ api/callback.js
@@ -1,3 +1,40 @@
-export default function handler(req, res) {
-  res.send("API CALLBACK OK");
+export default async function handler(req, res) {
+  const code = req.query.code;
+
+  if (!code) {
+    return res.status(400).send("No code provided");
+  }
+
+  const data = new URLSearchParams({
+    client_id: process.env.DISCORD_CLIENT_ID,
+    client_secret: process.env.DISCORD_CLIENT_SECRET,
+    grant_type: "authorization_code",
+    code,
+    redirect_uri: process.env.DISCORD_REDIRECT_URI,
+    scope: "identify"
+  });
+
+  const tokenRes = await fetch("https://discord.com/api/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: data
+  });
+
+  const token = await tokenRes.json();
+
+  const userRes = await fetch("https://discord.com/api/users/@me", {
+    headers: {
+      Authorization: `Bearer ${token.access_token}`
+    }
+  });
+
+  const user = await userRes.json();
+
+  res.send(`
+    <h1>Connected</h1>
+    <p>ID: ${user.id}</p>
+    <p>Username: ${user.username}#${user.discriminator}</p>
+  `);
 }
--- api/login.js
+++ api/login.js
@@ -1,3 +1,12 @@
 export default function handler(req, res) {
-  res.send("API LOGIN OK");
+  const params = new URLSearchParams({
+    client_id: process.env.DISCORD_CLIENT_ID,
+    redirect_uri: process.env.DISCORD_REDIRECT_URI,
+    response_type: "code",
+    scope: "identify"
+  });
+
+  res.redirect(
+    "https://discord.com/api/oauth2/authorize?" + params.toString()
+  );
 }
